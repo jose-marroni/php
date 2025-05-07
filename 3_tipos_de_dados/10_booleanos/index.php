1b3bbf9e4655bac0d097586640a1a80299c010bf
joseMarroni98 <marroni55@outlook.com>
2025-05-07 15:03:17 -0300

    Create index.php
    create index.php

---
+++ 3_tipos_de_dados/10_booleanos/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo true;
+    echo"<br>";
+    echo false;
+
+    if(true){
+        echo"É verdadeiro <br>";
+    }
+
+    $podeEntrar = true;
+
+    if($podeEntrar){
+        echo" O usurario pode entrar <br>";
+    }
