f2dff28ebd8f738842a75a98c167eecc1eaa0520
joseMarroni98 <marroni55@outlook.com>
2025-05-08 13:01:01 -0300

    Create index.php
    create index.php

---
+++ 3_tipos_de_dados/7_strings/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "testando texto de aspas duplas <br>";
+    echo 'testando texto de aspas simples <br>';
+
+    echo "Ele disse: 'Olá!' <br>";
+    echo 'Ele disse: "Olá" <br>';
+
+    $idade = 15;
+
+    echo "Ele tem $idade anos <br>";
+    echo 'Ele tem $idade anos <br>';
