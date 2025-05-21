efd1187f046c5d2d97b1f46b1914295a30ee97e3
joseMarroni98 <marroni55@outlook.com>
2025-05-21 20:47:25 -0300

    Create index.php
    create index.php

---
+++ 3_tipos_de_dados/6_exercicio_4/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $a =12.3;
+
+    echo $a;
+    echo "<br>";
+
+    echo 13.45;
+    echo "<br>";
+
+    echo -13.45;
+    echo "<br>";
+
+    if(is_float(123.54)){
+        echo "É um float";
+    }
+
+?>
