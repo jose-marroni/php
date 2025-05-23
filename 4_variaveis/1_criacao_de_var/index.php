084a144bf8b6617c4f20db670c5097f5255fd692
joseMarroni98 <marroni55@outlook.com>
2025-05-22 21:32:36 -0300

    Create index.php
    create index.php

---
+++ 4_variaveis/1_criacao_de_var/index.php
@@ -0,0 +1,18 @@
+<?php
+$teste = "algum texto";
+echo $teste;
+
+$num = 10;
+echo'<br>';
+
+echo $num;
+
+$_nome = "matheus";
+
+echo '<br>';
+echo $_nome;
+
+echo '<br>';
+
+$velocidadeMaxima = 100;
+$velocidade_minima = 100;
