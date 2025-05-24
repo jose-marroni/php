bd74a5e137417636fe50b594d40f4c0a4734a8b1
joseMarroni98 <marroni55@outlook.com>
2025-05-23 21:01:41 -0300

    Create index.php
    create index.php

---
+++ 4_variaveis/2_exercicio_8/index.php
@@ -0,0 +1,10 @@
+<?php
+$velociadade = 100;
+$marca = "ferrari";
+$itens = ["teto solar", "motor 2.0", "Porta malas grande", "Piloto automotico "];
+
+echo $velociadade;
+echo "<br>";
+echo $marca;
+echo "<br>";
+print_r($itens);
