51cc760f9041ae52e2174bce811923e5f167577c
joseMarroni98 <marroni55@outlook.com>
2025-05-27 23:16:57 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/10_op_concatenacao/index.php
@@ -0,0 +1,19 @@
+<?php
+echo "testando a concatenação";
+echo"<br>";
+
+echo "testando"." a ". "concatenação";
+echo"<br>";
+
+$t = "testando";
+$c = "concatenação";
+
+echo $t . " a ". $c;
+echo"<br>"; 
+
+$marca = "ferrari";
+$motor = "3.0";
+$vel_max = 200;
+
+echo "O carro da ". $marca . " tem um motor ". $motor . " e chaga uma velocidade de ". $vel_max . "km/h";
+echo "<br>";
