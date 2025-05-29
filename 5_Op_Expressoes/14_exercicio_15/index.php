483ee3b0a9a8c04d87d1069a0afbb115676c0dde
joseMarroni98 <marroni55@outlook.com>
2025-05-29 18:56:27 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/14_exercicio_15/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = "luiz";
+$nomeUse = "luiz";
+$nomeUse2 = "tobi";
+
+if($nome == $nomeUse){
+
+    echo "Esse nome pertence a esse usuario 1 <br>";
+
+}
+if($nome == $nomeUse2){
+      echo "Esse nome pertence a esse usuario <br>";
+}
+
