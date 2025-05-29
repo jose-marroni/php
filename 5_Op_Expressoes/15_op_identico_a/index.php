e2d111bf8b4c37974905aa581a579b1e4068b140
joseMarroni98 <marroni55@outlook.com>
2025-05-29 18:56:32 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/15_op_identico_a/index.php
@@ -0,0 +1,28 @@
+<?php
+if(5 == "5"){
+
+    echo " 5 é 5 1 <br>";
+}
+
+if(5 === 5){
+
+    echo " 5 é 5 2 <br>";
+}
+if(5 === 5){
+
+    echo " 5 é 5 3 <br>";
+}
+
+$a = 4;
+$b = 4;
+$c = 5;
+
+if($a === $b){
+
+echo "A é igual a B <br>";
+}
+
+if($a === $c){
+
+echo "A é igual a c <br>";
+}
