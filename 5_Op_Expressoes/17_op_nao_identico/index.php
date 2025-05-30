8acb120686a0fb551b9c8f998a3798161b34a3d9
joseMarroni98 <marroni55@outlook.com>
2025-05-30 19:17:32 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/17_op_nao_identico/index.php
@@ -0,0 +1,28 @@
+<?php
+$a = 4;
+$b = "4";
+$c = 5;
+
+if($a != $b){
+
+echo "A é diferente a B 1 <br>";
+}
+
+if($a !== $b){
+
+echo "A é diferente a b 2 <br>";
+}
+
+if(1 !== 2){
+
+echo "Não é identico ! <br>";
+}
+
+if(1 !== "1"){
+
+echo "Não é identico ! <br>";
+}
+if([] !== 2){
+
+echo "Não é identico ! <br>";
+}
