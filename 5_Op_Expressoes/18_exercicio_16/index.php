38f4574a0a7961f9c19066dec0ef2ec3ed7086ce
joseMarroni98 <marroni55@outlook.com>
2025-05-30 19:17:36 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/18_exercicio_16/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$x = 5;
+$y = 3;
+
+if($x == $y ){
+    echo "Valores sao iguais <br>";
+}
+
+if($x != $y ){
+    echo "Valores sao difierentes <br>";
+}
+
+if($x === $y ){
+    echo "Valores sao identicos <br>";
+}
+if($x !== $y ){
+    echo "Valores não são identicos  <br>";
+}
