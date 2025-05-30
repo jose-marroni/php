8b9ebe40cae8cf0609bf598c7e56bd24b3fa0f8f
joseMarroni98 <marroni55@outlook.com>
2025-05-30 19:17:27 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/16_op_diferenca/index.php
@@ -0,0 +1,26 @@
+<?php
+
+if(5 != "5"){
+
+    echo " 5 deferente 5 1 <br>";
+}
+
+if(5 != 6){
+
+    echo " 5 diferente 6 2 <br>";
+}
+
+
+$a = 4;
+$b = 4;
+$c = 5;
+
+if($a != $b){
+
+echo "A é diferente a B <br>";
+}
+
+if($a != $c){
+
+echo "A é diferente a c <br>";
+}
