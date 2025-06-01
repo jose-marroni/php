283773a00ff6b1cfa29b64db8caca3d9c2e474f5
joseMarroni98 <marroni55@outlook.com>
2025-06-01 16:39:45 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/20_op_menor_ou_igual_que/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 4;
+$b = 5;
+$c = 6;
+$d = 6;
+
+if($a <= $b){
+    echo "A é menor que B <br>";
+}
+
+if($b => $a){
+    echo "B é menor que A <br>";
+}
+
+if($c <= $d){
+    echo "C é menor ou igual que D <br>";
+}
