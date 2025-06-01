edb44b9681f10d4283104e8fb1eb5cbd872eee6c
joseMarroni98 <marroni55@outlook.com>
2025-06-01 16:39:34 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/19_op_maior_ou_igual_que/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 4;
+$b = 5;
+$c = 6;
+$d = 6;
+
+if($a > $b){
+    echo "A é maior que B <br>";
+}
+
+if($b > $a){
+    echo "B é maior que A <br>";
+}
+
+if($c >= $d){
+    echo "C é maior ou igual que D <br>";
+}
