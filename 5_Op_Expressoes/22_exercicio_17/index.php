40fd5496324df586fc52e37c1f8c31c070b3ebb7
joseMarroni98 <marroni55@outlook.com>
2025-06-02 21:06:57 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/22_exercicio_17/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if(15 > 5 && "João" === "João"){
+    echo "condição verdadeira 1 <br>";
+}
+
+if("teste" > 5 && 1){
+    echo "condição verdadeira 2<br>";
+}
+
+if(2 == 3 && 5 >= 3){
+    echo "condição verdadeira 3 <br>";
+}
