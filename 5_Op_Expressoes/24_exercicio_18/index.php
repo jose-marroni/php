816c3ea0b574a0fce43708ec0515d78e5597903e
joseMarroni98 <marroni55@outlook.com>
2025-06-03 20:36:21 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/24_exercicio_18/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if(12 < 5 || "João" ==="João"){
+    echo "operação 1 verdadeira <br>";
+}
+
+if(1 > 5 || 1){
+    echo "operação 2 verdadeira <br>";
+}
+
+if( 20 === "20" && 51>=31){
+    echo "operação 3 verdadeira <br>";
+}
