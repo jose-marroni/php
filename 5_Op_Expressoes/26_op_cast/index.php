eac263b6b50796392e3740ced76da32ef5a3ed0e
joseMarroni98 <marroni55@outlook.com>
2025-06-03 20:36:33 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/26_op_cast/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = (int) 12;
+echo "<br>";
+
+echo $a + 10;
+
+if($a === 12){
+    echo "A é identico a 12 <br>";
+}
+
+$b = (float) 3.1484;
+
+if($b === 3.1484){
+    echo "B é identico a 3.1484 <br>";
+}
+
+$c = (string) 34;
+if($c === "34"){
+    echo "C é identico a 34 <br>";
+}
