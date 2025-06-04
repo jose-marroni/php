ab9863cf220e5c27be306e42298bec3fcea7d2be
joseMarroni98 <marroni55@outlook.com>
2025-06-04 19:42:11 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/27_exercicio_19/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$a =(int)"testando";
+echo $a . "<br>";
+
+$b =(int)"asd";
+echo $b . "<br>";
+
+$c =(int) 12.9;
+echo $c . "<br>";
+
+$d =(int) true;
+echo $d . "<br>";
+
+
+$e =(int) [1,2,3];
+echo $e . "<br>";
