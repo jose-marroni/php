93fb9804f6c3b71dfe5c11f56944d725fd5ae41e
joseMarroni98 <marroni55@outlook.com>
2025-06-10 20:38:53 -0300

    Create index.php
    create index.php

---
+++ 5_Op_Expressoes/7_Operador_modulo/index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 12 % 2;
+echo "<br> modúlo <br>";
+
+echo 12 / 2;
+echo "<br> Divisão <br>";
+
+
+echo 13 % 2;
+echo "<br>";
+echo 13 % 5;
+echo "<br>";
+
+$a = 20;
+$b = 4;
+$c = 12;
+
+
+echo $a % $b;
+echo "<br>";
+echo $a % $c;
