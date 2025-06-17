a2c558dbbfea9b2e5196e7b05bc43751d54a9213
joseMarroni98 <marroni55@outlook.com>
2025-06-17 20:39:22 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/11_exercico_25/index.php
@@ -0,0 +1,44 @@
+<?php
+
+$vl = 40;
+$vl2 = 36;
+$vl3 = 41;
+
+if($vl < 40 ){
+
+    echo "A velocida esta dentro do limite <br>";
+
+}else if($vl == 40 ){
+
+    echo "Cuidado velocidade perde de atingir o limite <br>";
+
+
+}else if($vl > 40){
+    echo "Acima do limite, multado <br>";
+}
+
+if($vl2 < 40 ){
+
+    echo "A velocida esta dentro do limite <br>";
+
+}else if($vl2 == 40 ){
+
+    echo "Cuidado velocidade perde de atingir o limite <br>";
+
+
+}else if($vl2 > 40){
+    echo "Acima do limite, multado <br>";
+}
+
+if($vl3 < 40 ){
+
+    echo "A velocida esta dentro do limite <br>";
+
+}else if($vl3 == 40 ){
+
+    echo "Cuidado velocidade perde de atingir o limite <br>";
+
+
+}else if($vl3 > 40){
+    echo "Acima do limite, multado <br>";
+}
