9a2d23ec4025b4e463094ff7a489b5f6610302d5
joseMarroni98 <marroni55@outlook.com>
2025-06-16 21:17:39 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/10_else_if/index.php
@@ -0,0 +1,49 @@
+<?php
+
+if(10 > 5){
+
+    echo "Entrou no primeiro if <br>";
+
+}else if(10 > 6){
+ 
+    echo "Entrou so else if<br>";
+
+}
+
+if(10 < 5){
+
+    echo "Entrou no primeiro if 2 <br>";
+
+}else if(10 > 6){
+ 
+    echo "Entrou so else if 2 <br>";
+
+}
+
+if(10 < 5){
+
+    echo "Entrou no primeiro if 3 <br>";
+
+}else if(10 < 6){
+ 
+    echo "Entrou so else if 3<br>";
+
+}else{
+    echo "Entrou no else 3<br>";
+}
+
+if(10 < 5){
+
+    echo "Entrou no primeiro if 4 <br>";
+
+}else if(10 < 6){
+ 
+    echo "Entrou so else if 4<br>";
+
+}else if(10 > 6){
+ 
+    echo "Entrou so else if 4<br>";
+
+}else{
+    echo "Entrou no else 4<br>";
+}
