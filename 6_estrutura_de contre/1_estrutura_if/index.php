413220a4f23ef9b4aee9cead3fef3fc2ab5e305a
joseMarroni98 <marroni55@outlook.com>
2025-06-19 21:42:30 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/1_estrutura_if/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//condição  verdadeira
+
+if( 5 > 2 ){
+    echo " Deu certo! entrou no if <br>";
+}
+
+// condição falsa
+if( 5 < 2 ){
+    echo " Deu certo! entrou no if 2 <br>";
+}
+
+
+if( 10 === 10 && 9>3 ){
+    echo " Deu certo! entrou no if 3 <br>";
+}
+
+
+//variavel
+$a = 10;
+$b = 5;
+
+
+if( $a >= $b  ){
+    echo " Deu certo! entrou no if 4 <br>";
+}
