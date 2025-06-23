b695b824f3ccdb34a04a262b10c67be1a4ca4b6f
joseMarroni98 <marroni55@outlook.com>
2025-06-22 22:22:27 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/2_exercicio_20/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 5;
+$b = 2;
+
+if($a > $b){
+    echo"5 é maior que 2 <br>";
+
+}
+
+$nome1 = "matheus";
+$nome2 = "pedro";
+
+if($nome1 != $nome2){
+    echo "Os nomes são diferentes <br>";
+}
+
+$c = 12;
+$d = 11;
+
+
+if( $c <= $d  ){
+    echo " 12 nao é menor ou igual a 11 <br>";
+}
+
