443311a2ede689b6090873796b22f81d69c57c46
joseMarroni98 <marroni55@outlook.com>
2025-06-23 21:00:19 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/3_exercicio_21/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$idade = 16;
+$idade2 = 18;
+$idade3 = 20;
+
+if($idade >= 18){
+    echo "1 - ";
+    echo "maior de idade <br>";
+}
+
+if($idade2 >= 18){
+    echo "2 - ";
+    echo "maior de idade <br>";
+}
+
+if($idade3 >= 18){
+    echo "3 - ";
+    echo "maior de idade <br>";
+}
