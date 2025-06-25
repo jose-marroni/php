d79b6da34e1047dc236e41ecb1af13a4367e4e06
joseMarroni98 <marroni55@outlook.com>
2025-06-25 19:39:15 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/4_else/index.php
@@ -0,0 +1,32 @@
+<?php
+
+if(5 > 2){
+    echo "entrou no if <br>";
+}else{
+    echo "entrou no else <br>";
+}
+
+if("teste" === 5){
+    echo "entrou no if <br> ";
+}else{
+    echo "entrou no else <br>";
+}
+
+//variaveis
+
+$a = 10;
+$b = 20;
+
+if($a > $b){
+
+}else{
+    echo "entrou no esle 2 <br>";
+}
+
+$msg = "entrou no esle 3 <br>";
+
+if($a > $b){
+
+}else{
+    echo $msg;
+}
