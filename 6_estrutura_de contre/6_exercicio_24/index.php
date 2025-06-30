6306f22308dd4b530cb936b69dedcc08991f16ae
joseMarroni98 <marroni55@outlook.com>
2025-06-29 23:03:27 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/6_exercicio_24/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$str = "jose";
+$int = 5;
+$boole = true;
+
+if(is_int($int)  ){
+    echo "Essa variavel é inteira <br>";
+}else{
+    echo "Essa variavel não é inteira";
+}
+
+if(is_int($str) ){
+    echo "Essa variavel é inteira <br>";
+}else{
+    echo "Essa variavel não é inteira <br>";
+}
+
+if(is_int($boole)  ){
+    echo "Essa variavel é inteira <br>";
+}else{
+    echo "Essa variavel não é inteira <br>";
+}
