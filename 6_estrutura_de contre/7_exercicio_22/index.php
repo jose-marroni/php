d910bb5f1111ab8afa3ec03c5bd9110654f387c9
joseMarroni98 <marroni55@outlook.com>
2025-06-30 21:46:47 -0300

    Create index.php
    create index.php

---
+++ 6_estrutura_de contre/7_exercicio_22/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$peso = 81;
+$peso2 = 79;
+
+
+if($peso > 80){
+    echo "O peso esta pesado de mais <br>";
+}else{
+    echo "Peso dentro do limite <br>";
+}
+
+
+if($peso2 > 80){
+    echo "O peso esta pesado de mais <br>";
+}else{
+    echo "Peso dentro do limite <br>";
+}
