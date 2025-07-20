4bcbe3ed9bded656f82d83e9ba7c5d9c3a78ff7d
joseMarroni98 <marroni55@outlook.com>
2025-07-20 19:43:18 -0300

    Create index.php
    create index.php

---
+++ 7_estrutura_de_repeticao/12_exercico_30C/index.php
@@ -0,0 +1,14 @@
+<?php
+$arr = [];
+
+for($i = 10; $i <= 20; $i++ ){
+    array_push($arr , $i);
+}
+print_r($arr);
+echo "<br>";
+
+for($j = 0; $j < count($arr); $j ++){
+    if($arr[$j] % 2 != 0){
+        echo "Número $arr[$j] <br>";
+    }
+}
