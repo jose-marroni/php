30f0441f174426098a478676b3c7848756028607
joseMarroni98 <marroni55@outlook.com>
2025-07-22 23:52:50 -0300

    Create index.php
    create index.php

---
+++ 7_estrutura_de_repeticao/1_while/index.php
@@ -0,0 +1,44 @@
+<?php
+
+// definição do contador
+$x =0;
+//inicio / Definição do contador 
+while($x < 10){
+    //Corpo do loop
+    echo $x . "<br>";
+    // Incremento do contador
+    $x++;
+}
+
+echo "Continuando o codigo <br>";
+
+$y =0;
+
+while($y <= 10){
+    echo $y . "<br>";
+    $y += 2;
+}
+
+echo "Continuando o codigo <br>";
+
+// definição do contador
+$z =10;
+//inicio / Definição do contador 
+while($z > 0){
+    //Corpo do loop
+    echo $z . "<br>";
+    // Incremento do contador
+    $z--;
+}
+echo "Continuando o codigo <br>";
+// definição do contador
+$a =10;
+//inicio / Definição do contador 
+while($a > 0){
+    //Corpo do loop
+    if($a % 2 != 0){
+    echo $a . "<br>";
+    }
+    // Incremento do contador
+    $a--;
+}
