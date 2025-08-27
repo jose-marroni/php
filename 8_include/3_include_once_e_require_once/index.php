4768034e41fc640e84c67d3a12982337ac855300
joseMarroni98 <marroni55@outlook.com>
2025-08-26 22:43:45 -0300

    Create index.php
    index.php

---
+++ 8_include/3_include_once_e_require_once/index.php
@@ -0,0 +1,15 @@
+<?php
+//Arquivos que nao existem
+//include_once "teste.php";
+
+//arquivos que existem
+include_once "teste2.php";
+include_once "teste2.php";
+
+//arquivo que nao existe
+//require_once "teste.php";
+
+require_once "teste3.php";
+?>
+
+<p>Testando código!</p>
