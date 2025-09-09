5bbe875167e18d4a7f22c2663c09f3647647f11a
joseMarroni98 <marroni55@outlook.com>
2025-09-08 23:11:39 -0300

    Create index.php
    create index.php

---
+++ 9_funções/10_exercicio_35/index.php
@@ -0,0 +1,13 @@
+<?php
+
+function quadrado($numero) {
+    return ($numero * $numero);
+}
+
+echo quadrado(5);
+
+function numAoQuadrado($num) {
+    return $num ** 2;
+}
+
+echo "<br>" . numAoQuadrado(10) . "<br>";
