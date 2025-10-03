e2adce361cbdeafa5ca10915db3261b8c73d9183
joseMarroni98 <marroni55@outlook.com>
2025-10-03 20:01:07 -0300

    Create index.php
    create index.php

---
+++ 9_funções/18_exercico_40/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$lista = ["Arroz", "Feijão", "Macarrão", "Carne", "Frango"];
+
+function listaParaString($arr){
+    $str = "Você levou estes itens do mercado: ";
+    for($i = 0; $i < count($arr); $i++){
+        if($i + 1 == count($arr)){
+            $str .= $arr[$i] . ".";
+        } else {
+            $str .= $arr[$i] . ", ";
+        }
+    }
+    return $str;
+}
+
+echo listaParaString($lista);
