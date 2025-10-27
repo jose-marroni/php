b6b261d1b900a5fde1a49cb2362ee411b0524519
joseMarroni98 <marroni55@outlook.com>
2025-10-27 13:30:30 -0300

    Create index.php
    create index.php

---
+++ 9_funções/21_exercico_41/index.php
@@ -0,0 +1,15 @@
+<?php
+
+function isPrime ($number) {
+    if ($number <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+echo isPrime(7);
