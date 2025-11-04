47e80148edea56a9181fca4aaa8c758ee186cec0
joseMarroni98 <marroni55@outlook.com>
2025-11-03 23:07:59 -0300

    Create index.php
    create index.php

---
+++ 9_funções/2_chamando_funcao/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Definindo a função
+function soma(){
+    
+    $a = 2;
+    $b = 6;
+    
+    echo $a + $b . "<br>";
+}
+
+//chamando a função
+soma();
+
+// funcao do php
+
+echo strtoupper("teste de função") . "<br>";
