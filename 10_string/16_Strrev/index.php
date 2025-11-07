6215e55274529966c40550941a826c97e4c13b7e
joseMarroni98 <marroni55@outlook.com>
2025-11-07 17:11:12 -0300

    Create index.php
    create index.php

---
+++ 10_string/16_Strrev/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$palavra = "Testando";
+
+$palavraInvertida = strrev($palavra);   
+
+echo "A palavra original é: $palavra <br>";
+echo "A palavra invertida é: $palavraInvertida <br>";
+
+$frase = "O programador estava com o prazo curto para entragar o sistema";  
+$fraseInvertida = strrev($frase);
+
+echo "A frase original é: $frase <br>";
+echo "A frase invertida é: $fraseInvertida <br>";
