b317ae72e81903775a56b9caa524cf87826751d2
joseMarroni98 <marroni55@outlook.com>
2025-11-11 16:26:36 -0300

    Create index.php
    create index.php

---
+++ 10_string/1_interpolacao/index.php
@@ -0,0 +1,8 @@
+<?php
+$nome = "João";
+$idade = 20; 
+$profissao = "Programador";
+
+echo "Eu sou o $nome, tenho $idade anos e trabalho como $profissao.<br>";
+
+echo "Eu sou o {$nome} e tenho {$idade} anos, e atuo como {$profissao}.<br>"; 
