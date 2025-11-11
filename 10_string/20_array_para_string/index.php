58c0a19e2665dfe67da4b7a49257d2b8b8621024
joseMarroni98 <marroni55@outlook.com>
2025-11-11 16:26:41 -0300

    Create index.php
    create index.php

---
+++ 10_string/20_array_para_string/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = ["maça", "banana", "laranja", "uva"];
+
+$str = implode(", ", $arr);
+
+echo "$str <br>"; 
+
+$arr2 = ["aviao", "carro", "moto", "navio"];
+
+$str2 = implode(" <-> ", $arr2);
+
+echo "$str2 <br>";
