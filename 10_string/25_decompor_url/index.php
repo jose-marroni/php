a2b76d7a5970000adc6b4d5bba6d00a34cad999c
joseMarroni98 <marroni55@outlook.com>
2025-11-13 14:01:48 -0300

    Create index.php
    create index.php

---
+++ 10_string/25_decompor_url/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$url = 'https://www.google.com';
+
+$array_url = parse_url($url);
+
+print_r($array_url);    
+echo '<br>';
+
+echo $array_url['host'];
+echo '<br>';
+
+$url2 = "http://www.horadecodar.com.br/?busca=php";
+
+$array_url2 = parse_url($url2);
+
+print_r($array_url2);
+echo '<br>';
