e84a9159c7d6fb4eccb005140fe836835bcaae67
joseMarroni98 <marroni55@outlook.com>
2025-11-13 14:01:52 -0300

    Create index.php
    create index.php

---
+++ 10_string/2_escape/index.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Content-type: text/plain");    
+//pular linha
+
+echo "Isso aqui vai ficar na primerira linha \n e isso na segunda linha. \n";
+
+//tab
+echo "Testando o tab \t aqui. \n";
+
+//barra invertida
+echo "Isso é uma barra invertida: \\ \n";   
+
+//dolar
+
+echo "Isso é um símbolo de dólar: \$ \n";   
