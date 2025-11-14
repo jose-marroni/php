d7fb1f64d9a4fb9c8c9c354491e77810721f1850
joseMarroni98 <marroni55@outlook.com>
2025-11-14 15:28:32 -0300

    Create index.php
    create index.php

---
+++ 10_string/5_funcao_printf/index.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$nome = "João";
+
+//%s é uma string
+
+printf("Olá, %s! Bem-vindo ao PHP. <br>", $nome);
+
+//%d é um número inteiro
+
+$n = 10;
+printf("o primeiro numero é %d e o segundo numero é %d <br>", $n, 30);
+
+//%f -> float
+$preco = 19.99;
+printf("O preço do produto é R$ %.1f <br>", $preco);
